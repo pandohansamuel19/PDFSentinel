8d3ad044f0bb96b29149456f37ab0c3b730892f8
Samuel Pandohan Terampil Gultom <pandohansamuel19@gmail.com>
2023-09-05 23:17:12 +0700

    feat(app): Update Dependencies

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+import os
 from typing import List
 
 
-app = FastAPI()
+import streamlit as st
+import pandas as pd
+import tensorflow as tf
 
-@app.get("/")
-async def root() -> None:
-    return {"message": "Hello World"}
+# os.environ["TF_MIN_GPU_MULTIPROCESSOR_COUNT"] = "0"
+
+# check the GPU Precense, if this running CPU-Based, there will be some warning
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+# @tf.function
+# def apply_gradient(optimizer, loss_object, model, x, y):
+#     with tf.GradientTape() as tape:
+#         logits = model(x)
+#         loss_value = loss_object(y_true=y, y_pred=logits)
+  
+#     gradients = tape.gradient(loss_value, model.trainable_weights)
+#     optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+
+#     return logits, loss_value
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
